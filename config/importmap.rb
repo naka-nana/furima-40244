b4c46d6e3b18ae9eee0caa79e679ecf46a823dbb
naka-nana <harunanapanda@icloud.com>
2024-02-26 18:18:38 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin "item-price", to: "item_price.js"
+pin "item_price", to: "item_price.js"
 pin "card", to: "card.js"
