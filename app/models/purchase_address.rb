bc5afdb71e6c6a32e7faf7bb170a5a6960dc089c
naka-nana <harunanapanda@icloud.com>
2024-02-27 11:11:39 +0900

    修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,7 +3,7 @@ class PurchaseAddress
 
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
-  validates :item_id, :user_id, :postal_code,:prefecture_id, :city, :address, :phone_number, :token, presence: true
+  validates :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :token, presence: true
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :phone_number, length: { minimum: 10, maximum: 11, too_long: 'is too long (maximum is 11 characters)' }, format: { with: /\A\d{10,11}\z/ }
