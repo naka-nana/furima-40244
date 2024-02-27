f8e9dc9fc9f8828683163f403376e87a8257f207
naka-nana <harunanapanda@icloud.com>
2024-02-27 11:03:42 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
   end
 
   def redirect_user
-    if current_user.id == @item.user_id || @item.order.present?
+    if current_user.id == @item.user_id || @item.purchase_history present?
       redirect_to root_path
     end
   end
